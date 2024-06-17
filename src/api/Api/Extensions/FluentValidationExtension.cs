4a3d1a06b4accf91c20ca489a0268f68b21473c0
Douglas Aguiar <douglas.aguiar@mfrural.com.br>
2024-06-17 20:40:17 -0300

    Método de extensão para fluent validation

---
+++ src/api/Api/Extensions/FluentValidationExtension.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Api.Extensions
+{
+    [ExcludeFromCodeCoverage]
+    public static class FluentValidationExtension
+    {
+        public static IMvcBuilder AddFluentValidation(this IMvcBuilder services)
+        {
+            //services.AddFluentValidation(fv =>
+            //{
+
+            //});
+
+            return services;
+        }
+    }
+}
--- src/api/Api/Program.cs
+++ src/api/Api/Program.cs
@@ -1,11 +1,10 @@
+using Api.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddMvc(options => { }).AddFluentValidation();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-
 app.UseHttpsRedirection();
 
 var summaries = new[]
